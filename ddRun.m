9589216fbc40d45a14b11cbec6a1a3a5a57bcd7d
Dhiana Deva <dhiana.deva@gmail.com>
2013-12-23 05:45:59 -0200

    Adds other training sessions

--- ddRun.m
+++ ddRun.m
@@ -15,11 +15,21 @@ function nets = ddRun()
     nets = {};
 
     % Construct train session
+    train_session = 'simple_trainbfg';
+
+        % Setup net
+        trainFcn = 'trainbfg';
+        net = ddSetupNet(hiddenLayerSize, trainFcn);
+        net.trainParam.max_fail = 50;
+        nets = ddJob(train_session, net, inputs, targets);
+
+    % Construct train session
     train_session = 'simple_traingd';
 
         % Setup net
         trainFcn = 'traingd';
         net = ddSetupNet(hiddenLayerSize, trainFcn);
+        net.trainParam.epochs = 10000;
         nets = ddJob(train_session, net, inputs, targets);
 
     % Construct train session
@@ -29,6 +39,54 @@ function nets = ddRun()
         trainFcn = 'traingd';
         net = ddSetupNet(hiddenLayerSize, trainFcn);
         net.trainParam.lr = 0.3;
+        net.trainParam.max_fail = 50;
+        net.trainParam.epochs = 10000;
+        nets = ddJob(train_session, net, inputs, targets);
+
+    % Construct train session
+    train_session = 'simple_traingda';
+
+        % Setup net
+        trainFcn = 'traingda';
+        net = ddSetupNet(hiddenLayerSize, trainFcn);
+        net.trainParam.epochs = 10000;
+        nets = ddJob(train_session, net, inputs, targets);
+
+    % Construct train session
+    train_session = 'better_traingda';
+
+        % Setup net
+        trainFcn = 'traingda';
+        net = ddSetupNet(hiddenLayerSize, trainFcn);
+        net.trainParam.max_fail = 50;
+        net.trainParam.epochs = 10000;
+        nets = ddJob(train_session, net, inputs, targets);
+
+    % Construct train session
+    train_session = 'simple_traingdm';
+
+        % Setup net
+        trainFcn = 'traingdm';
+        net = ddSetupNet(hiddenLayerSize, trainFcn);
+        net.trainParam.epochs = 10000;
+        nets = ddJob(train_session, net, inputs, targets);
+
+    % Construct train session
+    train_session = 'better_traingdm';
+
+        % Setup net
+        trainFcn = 'traingdm';
+        net = ddSetupNet(hiddenLayerSize, trainFcn);
+        net.trainParam.max_fail = 50;
+        net.trainParam.epochs = 10000;
+        nets = ddJob(train_session, net, inputs, targets);
+
+    % Construct train session
+    train_session = 'simple_trainlm';
+
+        % Setup net
+        trainFcn = 'trainlm';
+        net = ddSetupNet(hiddenLayerSize, trainFcn);
         net.trainParam.max_fail = 50;
         nets = ddJob(train_session, net, inputs, targets);
 
