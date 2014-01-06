731ae0920ec4d279b7de9e09f6828a9ced76fc6f
Dhiana <dhiana.deva@gmail.com>
2014-01-06 03:13:31 -0200

    Reduces figures resolution so it fits best ppt slides

--- ddPlot.m
+++ ddPlot.m
@@ -11,22 +11,22 @@ function ddPlot(net, tr, inputs, targets, train_session)
     f1 = figure;
     plotperform(tr);
     filename = strcat(print_prefix, 'perform');
-    print(f1, '-dpng', filename);
- 
+    print(f1, '-dpng', '-r72', filename);
+
     f2 = figure;
     plottrainstate(tr);
     filename = strcat(print_prefix, 'trainstate');
-    print(f2, '-dpng', filename);
- 
+    print(f2, '-dpng', '-r72', filename);
+
     f3 = figure;
     plotconfusion(targets,outputs);
     filename = strcat(print_prefix, 'confusion');
-    print(f3, '-dpng', filename);
+    print(f3, '-dpng', '-r72', filename);
 
     f4 = figure;
     plotroc(targets,outputs);
     filename = strcat(print_prefix, 'roc');
-    print(f4, '-dpng', filename);
+    print(f4, '-dpng', '-r72', filename);
 
     %figure, ploterrhist(errors)
 
